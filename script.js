fce1a57b9f7103e289ec058d1c556e3a75c588bd
ace-brown <kazem_abdi@yahoo.com>
2023-06-11 22:15:51 +0200

    refactor: rolling the dice func

--- script.js
+++ script.js
@@ -4,13 +4,28 @@
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const dice = document.querySelector(".dice");
+const rollDiceBtn = document.querySelector("#roll-dice-btn");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 
 // Setting the initial state ---------
 score0El.textContent = 0;
-score0El.textContent = 0;
+score1El.textContent = 0;
 dice.classList.add("hidden");
 
-//  Hide the dice func --------------
-// dice.addEventListener('click', () => {
-
-//  })
+//  Rolling the dice func --------------
+rollDiceBtn.addEventListener("click", () => {
+  const randomDice = Math.trunc(Math.random() * 6) + 1;
+  dice.classList.remove("hidden");
+  dice.src = `./img/dice-${randomDice}.png`;
+  if (randomDice === 1) {
+    if (player0.classList.contains("player--active")) {
+      player0.classList.remove("player--active");
+      player1.classList.add("player--active");
+    } else if (player1.classList.contains("player--active")) {
+      player1.classList.remove("player--active");
+      player0.classList.add("player--active");
+    }
+  } else {
+  }
+});
