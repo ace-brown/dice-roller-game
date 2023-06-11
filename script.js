5a399a5d24e65169134de315fea67b7a2fb44388
ace-brown <kazem_abdi@yahoo.com>
2023-06-11 19:36:24 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Query selectors -------------------------------------------------------------------------
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+
+// Setting the initial state ---------
+score0El.textContent = 0;
+score0El.textContent = 0;
+dice.classList.add("hidden");
+
+//  Hide the dice func --------------
+// dice.addEventListener('click', () => {
+
+//  })
